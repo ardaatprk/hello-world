647a741c73526df720e2cd2fd379ee16c27450c7
ardaatprk <ardaatprk@gmail.com>
2023-06-22 11:37:20 +0300

    Update

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
-
-def asalSayilariBul(sayi1, sayi2):
-    for sayi in range(sayi1, sayi2+1):
+class ClassAsal :
+    def __init__(self) -> None:
+        pass
+    def asalSayilariBul(sayi1, sayi2):  
+     for sayi in range(sayi1, sayi2+1):
         if sayi > 1:
             for i in range(2, sayi):
                 if (sayi % i == 0):
