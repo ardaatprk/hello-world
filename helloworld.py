ce21ede45f9241339e4ad0e466455f0f86194488
ardaatprk <ardaatprk@gmail.com>
2023-07-03 18:21:37 +0300

    Asal sayıları bulan fonksiyonu terminal komutları ile açıkladım

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
+import argparse
 class ClassAsal :
     def asalSayilariBul(self, sayi1, sayi2):  
      for sayi in range(sayi1, sayi2+1):
@@ -7,11 +8,14 @@ class ClassAsal :
                     break
             else:
                 print(sayi)
-
+def main(): 
+    parser = argparse.ArgumentParser(description="Araliktaki asal  sayilar")
+    parser.add_argument('-s1', '--sayi1', type=int, help='1.Sayi' )
+    parser.add_argument('-s2', '--sayi2', type=int, help='2.Sayi' )
+    args=parser.parse_args()
 if __name__== '__main__':
     print("Asal Sayilar")
     sayi1 = int(input('sayi 1:'))
     sayi2 = int(input('sayi 2:'))
     p1 = ClassAsal()
-
     p1.asalSayilariBul(sayi1, sayi2)
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 import helloworld
+from helloworld import asalSayilariBul
 print("Asal sayilar2")
