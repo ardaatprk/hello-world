ef72454ed12a6d5f715d798e1961749091f2fa6d
ardaatprk <ardaatprk@gmail.com>
2023-07-04 15:50:37 +0300

    Asal sayılar bulan fonksiyon inputları argümanlardan alıyor

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 import argparse
-class ClassAsal :
+
+class ClassAsal():
     def asalSayilariBul(self, sayi1, sayi2):  
      for sayi in range(sayi1, sayi2+1):
         if sayi > 1:
@@ -8,14 +9,20 @@ class ClassAsal :
                     break
             else:
                 print(sayi)
+
 def main(): 
     parser = argparse.ArgumentParser(description="Araliktaki asal  sayilar")
-    parser.add_argument('-s1', '--sayi1', type=int, help='1.Sayi' )
-    parser.add_argument('-s2', '--sayi2', type=int, help='2.Sayi' )
+    parser.add_argument('-s1', '--sayi1', type=int, help='1.Sayi')
+    parser.add_argument('-s2', '--sayi2', type=int, help='2.Sayi')
     args=parser.parse_args()
-if __name__== '__main__':
-    print("Asal Sayilar")
-    sayi1 = int(input('sayi 1:'))
-    sayi2 = int(input('sayi 2:'))
+
+    print(f"arg1 is : {args.sayi1}") 
+    print(f"arg2 is : {args.sayi2}")
     p1 = ClassAsal()
-    p1.asalSayilariBul(sayi1, sayi2)
+    p1.asalSayilariBul(args.sayi1, args.sayi2)
+
+if __name__== '__main__':
+    main()
+    exit(0)
+    
+   
